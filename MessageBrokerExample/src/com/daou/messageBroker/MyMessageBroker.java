0f77484467d995b28f048189ec6db0708406930d
DESKTOP-AM12U9O\MinYoung <qo613@naver.com>
2020-10-19 23:27:19 +0900

    fix: server is also client

--- MessageBrokerExample/src/com/daou/messageBroker/MyMessageBroker.java
+++ MessageBrokerExample/src/com/daou/messageBroker/MyMessageBroker.java
@@ -7,9 +7,10 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class MyMessageBroker {
-    private HashMap<String, Publisher> publishers = new HashMap<>();
+    private HashMap<String, HashSet<Subscriber>> subscribers = new HashMap<>();
     private Publisher publisher;
     private ServerSocket brokerServerSocket;
 
@@ -28,44 +29,45 @@ public class MyMessageBroker {
 
             /* brokerServerSocket.listen */
 
-            //1. how can broker know if it's server -> register publisher
-            Socket serverSocket = brokerServerSocket.accept();
-            publisher = new Publisher(serverSocket);
-//            String topic = "";
-//            publishers.put(topic, publisher); //if null
-            System.out.println("server has been connected");
-
-            ReceiveTask receiveTask = new ReceiveTask(publisher);
-            Thread receiveThread = new Thread(receiveTask);
-            receiveThread.start();
-
-            acceptSubscriber();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /*
-    * accept subscribers
-    * */
-    private void acceptSubscriber() {
-        try {
-            if(publisher == null) return;
-
-            while (true) {
-                //2. broker have to manage subs by topic
+            while(true){
                 Socket acceptSocket = brokerServerSocket.accept();
-                subscribe(acceptSocket);
+//                String topic = "";
+//                publishers.put(topic, publisher); //if null
+                System.out.println("client has been connected");
+
+                ReceiveTask receiveTask = new ReceiveTask(new Publisher(acceptSocket));
+                Thread receiveThread = new Thread(receiveTask);
+                receiveThread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void subscribe(Socket socket){
+    private void subscribe(String topic, Socket socket){
         if(publisher == null) return;
 
         Subscriber sub = new Subscriber(socket);
         publisher.subscribe(sub);
     }
+
+    /*
+    * accept subscribers
+    * */
+//    private void acceptSubscriber() {
+//        try {
+//            if(publisher == null) return;
+//
+//            while (true) {
+//                //2. broker have to manage subs by topic
+//                Socket acceptSocket = brokerServerSocket.accept();
+//                String topic = "";
+//                subscribe(topic, acceptSocket);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
 }
